fc823775c230d4100b6ae553aefca4a43b9f806a
Jason Karcheski <jason.karcheski@co-operativebank.co.uk>
2024-10-17 14:20:56 +0100

    Initial Commit

---
+++ Architecture/ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureApp.swift
+//  Architecture
+//
+//  Created by Jason Karcheski on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Architecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Architecture
+//
+//  Created by Jason Karcheski on 17/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
